64bc4fea344de4f06b1a4909f8945552ddb5da12
giulianavolpi <g.volpi@uniandes.edu.co>
2025-03-11 15:19:09 -0500

    Simuladores

--- diagnosticapp/historiaClinica/models.py
+++ diagnosticapp/historiaClinica/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+import time
+import random
 
 # Create your models here.
 class Paciente(models.Model):
@@ -11,3 +13,16 @@ class Paciente(models.Model):
 
     def __str__(self):
         return '{}'.format(self.nombre)
+    
+    
+class HistoriaClinica(models.Model):
+    paciente_id = models.CharField(max_length=100)
+    es_menor_edad = models.BooleanField(default=False)
+    existe = models.BooleanField(default=True)
+
+    #Simulador de consulta de historia clínica
+    def consultar_historia(self):
+        time.sleep(random.uniform(0.05, 0.2))  
+        if self.existe and self.es_menor_edad:
+            return {"paciente_id": self.paciente_id, "datos": "Información simulada"}
+        return None
--- diagnosticapp/infoAdmin/models.py
+++ diagnosticapp/infoAdmin/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+import time
+import random
 
 # Create your models here.
 class infoAdmin(models.Model):
@@ -8,3 +10,8 @@ class infoAdmin(models.Model):
     
     def __str__(self):
         return '{}'.format(self.nombre)
+    
+    #Simulador de acceso a información de administrador
+    def validar_acceso(self):
+        time.sleep(random.uniform(0.05, 0.15))  
+        return self.tiene_acceso
