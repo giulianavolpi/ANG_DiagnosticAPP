74f7e71106c3dd04e5a494b9406ca81a9743c279
NataliaVillegasC <n.villegasc@uniandes.edu.co>
2025-03-13 20:00:02 -0500

    Image

--- diagnosticapp/urls.py
+++ diagnosticapp/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -25,3 +27,7 @@ urlpatterns = [
     path('health-check/', views.healthCheck),
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
