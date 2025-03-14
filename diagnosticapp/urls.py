575b56ca5ce84c3b3add0cff286b4bbfde90d600
NataliaVillegasC <n.villegasc@uniandes.edu.co>
2025-03-13 19:10:53 -0500

    Health Check

--- diagnosticapp/urls.py
+++ diagnosticapp/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from . import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('paciente.urls')),
+    path('health-check/', views.healthCheck),
+
 ]
